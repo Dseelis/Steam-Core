ac451eb449615e1a1764064016d9c85ae69e9563
dseelis <dseelis.x@gmail.com>
2026-04-30 10:56:21 +0300

    refactor1

--- src/main/java/com/steam/steamcore/SteamCore.java
+++ src/main/java/com/steam/steamcore/SteamCore.java
@@ -45,7 +45,7 @@ public class SteamCore {
     public static final String MODID = "steamcore";
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final String PACK_NAME = "SteamCreate 2";
-    public static final String PACK_VERSION = "2.1.0b";
+    public static final String PACK_VERSION = "2.1.1b";
 
     // REGISTRIES
     public static final DeferredRegister.Blocks BLOCKS =
